0b9cdeb261be9ccfb372ec1ba89ed1fa2159cfbc
swonjiny <swonjiny@gmail.com>
2023-12-09 09:59:38 +0900

    처음

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+
+<template>
+  <v-app >
+
+    <v-app-bar color="#375472" :elevation="0"  height="50">
+      <v-img src="public/theqoo_logo.png" max-height="30" max-width="30" class="mx-3"></v-img>
+
+      <v-btn size="small"  v-if="drawer">전체</v-btn>
+      <v-btn size="small"  v-if="drawer">HOT</v-btn>
+      <v-btn size="small"  v-if="drawer">스퀘어</v-btn>
+      <v-btn size="small"  v-if="drawer">뷰티</v-btn>
+      <v-btn size="small"  v-if="drawer">일상토크</v-btn>
+      <v-btn size="small"  v-if="drawer">케이돌토크</v-btn>
+      <v-btn size="small"  v-if="drawer">드영배</v-btn>
+      <v-btn size="small"  v-if="drawer">재펜</v-btn>
+      <v-spacer v-if="drawer"></v-spacer>
+      <v-text-field
+        variant="outlined"
+        density="compact"
+        flat
+        bg-color="#6c839f"
+        single-line
+        prepend-inner-icon="mdi-magnify"
+        placeholder="게시판 찾기"
+        hide-details
+
+        @focus="drawer=false"
+        @blur="drawer=true"
+      />
+      <span class="text-body-2 ml-2 mr-2">로그인</span>
+
+
+
+    </v-app-bar>
+    <v-system-bar class="text-white" color="#6c839f">
+      <v-spacer></v-spacer>
+
+      <v-icon class="mr-2">mdi-cog</v-icon>
+      내 즐겨찾기 관리
+    </v-system-bar>
+    <v-main class="bg-grey-lighten-1">
+      <v-container>
+        <RouterView />
+      </v-container>
+    </v-main>
+  </v-app>
+
+
+<!--  <header>-->
+<!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
+
+<!--    <div class="wrapper">-->
+<!--      <HelloWorld msg="You did it!" />-->
+
+<!--      <nav>-->
+<!--        <RouterLink to="/">Home</RouterLink>-->
+<!--        <RouterLink to="/about">About</RouterLink>-->
+<!--      </nav>-->
+<!--    </div>-->
+<!--  </header>-->
+
+<!--  <RouterView />-->
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { onUpdated, ref, watchEffect } from 'vue'
+
+const drawer = ref(true)
+
+window.addEventListener('resize' , () => {
+  const width =  window.innerWidth
+  if(width < 800) drawer.value = false
+  else drawer.value = true
+})
+
+watchEffect(()=>{
+  const width =  window.innerWidth
+  console.log(width)
+
+})
+</script>
+
+<style scoped>
+.v-input--density-compact {
+  --v-input-control-height: 30px;
+  --v-input-padding-top: 5px;
+  --v-field-padding-top: 0px;
+  --v-field-padding-bottom: 0px;
+}
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+
+const app = createApp(App)
+app.use(vuetify)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
